6795c6d985c115a4cc5317b09fe7e28974edbcb7
slinkous <53449377+slinkouswasalreadytaken@users.noreply.github.com>
2021-02-17 12:13:38 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,24 @@
+echo Checking for dependencies...
+
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+
+sleep 2
+
+echo No conflicts found
+echo Checking package contents...
+
+echo -ne '#####                     (33%)\r'
+echo -ne '#############             (66%)\r'
+sleep 2
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+
+echo Installing, this could take a moment...
+sleep 3
+
+firefox https://youtu.be/dQw4w9WgXcQ ; chromium https://youtu.be/dQw4w9WgXcQ ; midori https://youtu.be/dQw4w9WgXcQ ; epiphany https://youtu.be/dQw4w9WgXcQ ; edge https://youtu.be/dQw4w9WgXcQ
